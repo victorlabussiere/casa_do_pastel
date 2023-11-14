3b6581a558a347f77f567ece2f6eabd5fb0a8cda
Victor Labussiere <victor.labussiere95@gmail.com>
2023-11-14 16:47:25 -0300

    homepage component updated

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FunctionsService } from 'src/app/providers/browserFunctions/functions.service';
+import { CategoryService } from 'src/app/providers/categories/category.service';
+import { Category } from 'src/mock/categories';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categories!: Category[]
+
+  constructor(
+    private categoryServices: CategoryService,
+    private browserFunctions: FunctionsService
+  ) { }
+
+  private getAllCategories() {
+    return this.categoryServices.getAllCategories()
+  }
+
+  ngOnInit(): void {
+    this.getAllCategories()
+      .subscribe(categories => this.categories = categories)
+  }
 
 }
