7e37735ec0e6c0117f4da3be375ca39873127fd3
Victor Labussiere <victor.labussiere95@gmail.com>
2023-11-14 15:37:39 -0300

    homepage component added

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./app/components/pages/home/home.component";
+
+
+export const routes: Routes = [
+    { path: "", component: HomeComponent, title: "Casa do Pastel | Bem vindo!" }
+]
