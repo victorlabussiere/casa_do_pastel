7608b467358edf8e126f642c10d470496fc2d974
Victor Labussiere <victor.labussiere95@gmail.com>
2023-11-19 23:21:43 -0300

    YAGNI and templates refactor

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,24 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { PlatesService } from 'src/app/providers/plates/plates.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { Plate, PlateGroups, PlatesKinds } from 'src/types';
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
   @Input() plates!: Plate[]
-  @Input() groupFilter!: PlateGroups | string
+  @Input() groupFilter: PlateGroups | string = 'pastel'
   kindFilter: PlatesKinds = 'salgado'
 
   pastelList!: Plate[]
   tapiocaList!: Plate[]
   acaiList!: Plate[]
 
-  constructor(private platesServices: PlatesService) {
-    this._initPlates()
-    this._setLists()
-  }
+  constructor() { }
 
   setStyleByKind(kind: string) {
     const filterColors: any = {
@@ -47,24 +42,11 @@ export class MenuComponent {
     return group === this.groupFilter ? titles[group][kind] : 'Selecione um item'
   }
 
-  private _initPlates() {
-    this.platesServices.getAllPlates().subscribe(plates => {
-      this.plates = plates
-    })
-  }
-
-  private _setLists() {
-    this.pastelList = this.plates.filter(i => i.category.kind == this.kindFilter && i.category.group == 'pastel')
-    this.tapiocaList = this.plates.filter(i => i.category.kind == this.kindFilter && i.category.group == 'tapioca')
-    this.acaiList = this.plates.filter(i => i.category.group === 'acai')
-  }
-
   setKindFilter(kind: PlatesKinds) {
     if (this.groupFilter === 'acai') {
       this.kindFilter = 'doce'
     }
 
     this.kindFilter = kind
-    this._setLists()
   }
 }
